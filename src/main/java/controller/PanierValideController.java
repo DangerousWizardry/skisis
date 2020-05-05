9b243a45e34807383ed67ba87dcd9bbd5c2d6f37
LucasDuval <lucas.duval@sigmalis.ch>
2020-05-05 23:14:25 +0200

    Link product stock with amount ordered

--- src/main/java/controller/PanierValideController.java
+++ src/main/java/controller/PanierValideController.java
@@ -8,21 +8,36 @@ package controller;
 import comptoirs.model.dao.ClientFacade;
 import comptoirs.model.dao.CommandeFacade;
 import comptoirs.model.dao.LigneFacade;
+import comptoirs.model.dao.ProduitFacade;
 import comptoirs.model.entity.Client;
 import comptoirs.model.entity.Commande;
 import comptoirs.model.entity.Ligne;
 import comptoirs.model.entity.LignePanier;
 import comptoirs.model.entity.Panier;
+import comptoirs.model.entity.Produit;
 import comptoirs.model.entity.User;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.util.Date;
 import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.inject.Inject;
 import javax.mvc.Controller;
 import javax.mvc.Models;
 import javax.mvc.View;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -44,7 +59,9 @@ public class PanierValideController {
 	CommandeFacade commandeDao;
 		
 	@Inject
+	ProduitFacade produitDao;
 	
+	@Inject
 	LigneFacade ligneDao;
 	
 	@Inject
@@ -68,29 +85,52 @@ public class PanierValideController {
         
         @POST
         public Response validation(@FormParam("destinataire") String destinataire, @FormParam("adresse") String adresse, @FormParam("codePostal") String codePostal, @FormParam("ville") String ville, @FormParam("region") String region, @FormParam("pays") String pays){
-			Commande commande = new Commande();
-			Client client = clientDao.find(user.getCode());
-            commande.setClient(client);
-            commande.setDestinataire(destinataire);
-            commande.setAdresseLivraison(adresse);
-            commande.setCodePostalLivrais(codePostal);
-            commande.setVilleLivraison(ville);
-            commande.setRegionLivraison(region);
-            commande.setPaysLivraison(pays);
-            commande.setSaisieLe(new Date());
-			commande.setRemise(BigDecimal.ZERO);
-			commandeDao.create(commande);
-			LinkedList<Ligne> lignes = new LinkedList<Ligne>();
-			for (LignePanier ligne : panier.getLignesPanier()) {
-                Ligne nouvLigne = new Ligne(commande.getNumero(), ligne.getProduit().getReference());
-                nouvLigne.setQuantite(ligne.getQuantite());
-				ligneDao.create(nouvLigne);
-				lignes.add(nouvLigne);
-            }
-			commande.setLigneCollection(lignes);
-			commandeDao.edit(commande);
-			client.getCommandeCollection().add(commande);
-			clientDao.edit(client);
+			
+			try {
+				UserTransaction ts = (UserTransaction) new InitialContext().lookup("java:comp/UserTransaction");
+				Commande commande = new Commande();
+				Client client = clientDao.find(user.getCode());
+				commande.setClient(client);
+				commande.setDestinataire(destinataire);
+				commande.setAdresseLivraison(adresse);
+				commande.setCodePostalLivrais(codePostal);
+				commande.setVilleLivraison(ville);
+				commande.setRegionLivraison(region);
+				commande.setPaysLivraison(pays);
+				commande.setSaisieLe(new Date());
+				commande.setRemise(BigDecimal.ZERO);
+				commandeDao.create(commande);
+				//Début de la transaction
+				ts.begin();
+				LinkedList<Ligne> lignes = new LinkedList<Ligne>();
+				for (LignePanier ligne : panier.getLignesPanier()) {
+					Ligne nouvLigne = new Ligne(commande.getNumero(), ligne.getProduit().getReference());
+					nouvLigne.setQuantite(ligne.getQuantite());
+					ligneDao.create(nouvLigne);
+					lignes.add(nouvLigne);
+					//décrémentation du stock produit
+					Produit produit = produitDao.findByReference(ligne.getProduit().getReference());
+					produit.setUnitesCommandees((short) (produit.getUnitesCommandees()+ligne.getQuantite()));
+					produit.setUnitesEnStock((short) (produit.getUnitesEnStock()-ligne.getQuantite()));
+				}
+				commande.setLigneCollection(lignes);
+				commandeDao.edit(commande);
+				client.getCommandeCollection().add(commande);
+				clientDao.edit(client);
+				//Fin de la transaction
+				ts.commit();
+			} catch (HeuristicMixedException ex) {
+				Logger.getLogger(PanierValideController.class.getName()).log(Level.SEVERE, null, ex);
+			} catch (HeuristicRollbackException ex) {
+				Logger.getLogger(PanierValideController.class.getName()).log(Level.SEVERE, null, ex);
+			} catch (SecurityException ex) {
+				Logger.getLogger(PanierValideController.class.getName()).log(Level.SEVERE, null, ex);
+			} catch (IllegalStateException ex) {
+				Logger.getLogger(PanierValideController.class.getName()).log(Level.SEVERE, null, ex);
+			}
+			catch(Exception ex){
+				Logger.getLogger(PanierValideController.class.getName()).log(Level.SEVERE, null, ex);
+			}
 			panier.clear();
 			return Response.seeOther(URI.create("/skisis/app/profil")).build();
         }
